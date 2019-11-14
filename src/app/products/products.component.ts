7596a1748b50fb1226e6f69dffb18b4d33b30791
yasikahivin <38648741+yasikahivin@users.noreply.github.com>
2019-11-14 21:22:47 +0530

    Revert "Revert "product and menu cmponents""
    This reverts commit 5b72bcaca9d3943575a3a90b06689c0029799e6d.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
+import { Product } from '../models/Product';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe(products => this.products = products);
   }
 
 }
