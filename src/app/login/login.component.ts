a25711ecd2f35998e90f62b55968d2b321703336
yasikahivin <38648741+yasikahivin@users.noreply.github.com>
2019-11-14 21:22:53 +0530

    Revert "Revert "sign-in""
    This reverts commit d89b67fc760dfc7b0519dc1911247164cd772b8b.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from '../auth.service';
-import { Routes } from '@angular/router';
-
+import { Router } from '@angular/router';
+import { NgFlashMessageService } from 'ng-flash-messages';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent  {
+
+export class LoginComponent  implements OnInit{
+  email: string;
+  password: string;
 
   constructor(
     private auth: AuthService,
     private db: AngularFirestore,
+    private router: Router,
+    private flashMessage: NgFlashMessageService
     ) {
   }
 
+  ngOnInit() {
+
+  }
 
   login() {
     this.auth.login();
   }
+
+  onSubmit(){
+    this.auth.login2(this.email, this.password)
+      .then(res => {
+        this.flashMessage.showFlashMessage({
+          messages:['You are now logged in'], 
+          type: 'success', 
+          timeout: 4000
+        });
+        this.router.navigate(['/admin/products/new']);
+      })
+        .catch(err => {
+          this.flashMessage.showFlashMessage({
+            messages:['Invalid'], 
+            type: 'danger', timeout: 4000
+        });
+  });
+}
 
 }
