abf5dfdf7ba8161c2e4f785218eb844dd837bd82
OshiWindsor <oshiwin@gmail.com>
2019-11-17 21:51:10 +0530

    login fixed

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { NgFlashMessageService } from 'ng-flash-messages';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private db: AngularFirestore,
+    private router: Router,
+    private flashMessage: NgFlashMessageService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.auth.register(this.email, this.password)
+    .then(res => {
+      this.flashMessage.showFlashMessage({
+        messages:['You are now logged in'], 
+        type: 'success', 
+        timeout: 4000
+      });
+      this.router.navigate(['/menu']);
+    })
+      .catch(err => {
+        this.flashMessage.showFlashMessage({
+          messages:['Invalid'], 
+          type: 'danger', timeout: 4000
+      });
+});
   }
 
 }
