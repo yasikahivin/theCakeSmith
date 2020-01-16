15d78c597c72153b10756af3df24eaca3cc99e14
OshiWindsor <36620242+OshiWindsor@users.noreply.github.com>
2020-01-16 23:07:27 +0530

    Path updates

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
+import { ProductService } from '../../services/product.service';
 import { Subscription } from 'rxjs';
-import { Product } from 'src/app/models/Product';
+import { Product } from '../../models/Product';
 
 @Component({
   selector: 'app-admin-products',
