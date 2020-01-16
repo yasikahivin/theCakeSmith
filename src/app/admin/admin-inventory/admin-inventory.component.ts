aca20948c296e752c6c9baec7feeb4903a79cc72
OshiWindsor <36620242+OshiWindsor@users.noreply.github.com>
2020-01-16 23:07:04 +0530

    Admin Inventory Component

---
+++ src/app/admin/admin-inventory/admin-inventory.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../../services/inventory.service';
+import { Subscription } from 'rxjs';
+import { Inventory } from '../../models/Inventory';
+
+@Component({
+  selector: 'app-admin-inventory',
+  templateUrl: './admin-inventory.component.html',
+  styleUrls: ['./admin-inventory.component.scss']
+})
+export class AdminInventoryComponent implements OnInit {
+  inventories: Inventory[] ;
+  subscription: Subscription;
+  filteredinventories: any[];
+
+  constructor(private inventoryService: InventoryService) {
+    this.subscription = this.inventoryService.getall()
+      .subscribe(inventories => this.filteredinventories = this.inventories = inventories);
+   }
+  
+  filter(query: string) {
+    this.filteredinventories = (query) ?
+    this.inventories.filter(i => i.name.toLowerCase().includes(query)) :
+    this.inventories;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit() {
+  }
+
+}
