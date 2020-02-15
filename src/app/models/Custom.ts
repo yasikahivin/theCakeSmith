6a9edc62ac0d7fda9f1e42a93d98af5bb2c0b322
OshiWindsor <36620242+OshiWindsor@users.noreply.github.com>
2020-02-15 15:07:29 +0530

    Custom

--- src/app/models/Custom.ts
+++ src/app/models/Custom.ts
@@ -1,10 +1,11 @@
 export interface Custom {
     id?: string;
     uid?: string;
+    uname?: string;
     flavor?: string;
     frosting_type?: string;
     frosting_color?: string;
-    weight?: number; 
+    weight?: number;
     shape?: string;
     wording?: string;
     imageURL?: string;
