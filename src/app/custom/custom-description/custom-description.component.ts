77b7ce06d06b51534e8887f5c018f2c6601911e7
nasra-nayeem <fathimanasra61@yahoo.com>
2020-02-17 15:19:17 +0530

    policies-component

---
+++ src/app/custom/custom-description/custom-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-description',
+  templateUrl: './custom-description.component.html',
+  styleUrls: ['./custom-description.component.scss']
+})
+export class CustomDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
