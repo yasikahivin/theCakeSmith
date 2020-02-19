a5b63e370ef8ed94d380eda9b0d2e5a42a3400b5
yasikahivin <38648741+yasikahivin@users.noreply.github.com>
2020-02-19 07:14:26 +0530

    cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -6,6 +6,10 @@ import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ShoppingCartItem } from '../models/shopping-cart-item';
+import{ ShoppingCart} from "../models/shopping-cart";
+import { map } from 'rxjs/operators';
+import { AppUser } from '../models/app-user';
+
 
 @Component({
   selector: 'app-shopping-cart',
@@ -13,12 +17,35 @@ import { ShoppingCartItem } from '../models/shopping-cart-item';
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
-  cart$;
+  cart: ShoppingCart;
+  appUser: AppUser;
 
-  constructor(private shoppingCartService: ShoppingCartService) { }
+  constructor(private shoppingCartService: ShoppingCartService,
+              auth: AuthService) { }
 
   async ngOnInit() {
-    this.cart$ = await this.shoppingCartService.getCart();
+
+    const cart$ = await this.shoppingCartService.getCart();
+    // await (await this.shoppingCartService.getCart()).valueChanges().pipe(map(res=>res as ShoppingCart)).subscribe(res=>{
+    //   console.log(res);this.cart=res});
+    // console.log ()
+
+
+  //  this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+
+// tslint:disable-next-line: align
+// console.log  (
+//     cart$.valueChanges().subscribe(cart => {
+//         for (const productId of Object.keys (cart.items)) {
+//           this.cart.items[productId].;
+//         }
+//       }))
+
+    // cart$.valueChanges().subscribe((cart => {
+    //   for (const productId of Object.keys (cart.items)) {
+    //     this.shoppingCartItemCount += cart.items[productId].quantity;
+    //   }
+    // }));
   }
 
   clearCart() {
